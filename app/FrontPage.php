0700a6e32da871958c37d38a12fa478726ea112d
Mike Kadin <michaelkadin@gmail.com>
2013-04-19 21:29:00 -0400

    gmaps api key in twig template

--- app/FrontPage.php
+++ app/FrontPage.php
@@ -7,5 +7,6 @@ use Silex\Application;
 function frontPage(Application $app) {
   return $app['twig']->render('page.twig', array(
     'title' => 'Favorite Locations!',
+    'gmaps_api_key' => $app['gmaps.api_key'],
   ));
 }
